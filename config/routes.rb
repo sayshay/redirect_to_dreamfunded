fe2a450da6c8a0ccb9bfb7e0d6469f1039f6f5c1
alexander <alexandr.larionov88@gmail.com>
2016-09-13 16:00:35 -0700

    all routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-  match '/:anything' => redirect("https://dreamfunded.com/"), via: 'get'
+  get '*path' => redirect("https://dreamfunded.com/")
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
